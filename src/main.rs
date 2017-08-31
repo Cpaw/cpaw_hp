d77e68d0c579c33ea50c07b3dc1b78b7ba8107b1
Chihiro Hasegawa <pgm3rdlinuxor1000@gmail.com>
2017-08-31 23:33:59 +0900

    とりあえず起動したやつ

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+extern crate iron;
+extern crate router;
+extern crate handlebars_iron as hbs;
+extern crate params;
+
+use std::collections::HashMap;
+use std::error::Error;
+use iron::prelude::*;
+use iron::status;
+use router::{Router, url_for};
+use hbs::{Template, HandlebarsEngine, DirectorySource};
+
+fn main() {
+    
+    fn top_handler(req: &mut Request) -> IronResult<Response> {
+        
+        let mut resp = Response::new();
+        let mut data = HashMap::new();
+        
+        data.insert(String::from("greeting_path"),
+                    format!("{}", url_for(req, "greeting", HashMap::new())));
+        
+        resp.set_mut(Template::new("index", data)).set_mut(status::Ok);
+        return Ok(resp);
+    }
+    
+    fn greet_handler(req: &mut Request) -> IronResult<Response> {
+        
+        use params::{Params, Value};
+        
+        let map = req.get_ref::<Params>().unwrap();
+        
+        return match map.find(&["name"]) {
+            
+            Some(&Value::String(ref name)) => {
+                Ok(Response::with(
+                    (status::Ok,
+                                         format!("Hello {}", name).as_str())
+                ))
+                                
+            },
+            _ => Ok(Response::with((status::Ok, "Hello world")))
+        }
+    }
+
+    //Create Router
+    let mut router = Router::new();
+    router.get("/", top_handler, "index");
+    router.post("/greet", greet_handler, "greeting");
+    
+    //Create Chain
+    let mut chain = Chain::new(router);
+    // Add HandlerbarsEngine to middleware Chain
+    let mut hbse = HandlebarsEngine::new();
+    hbse.add(Box::new(
+        DirectorySource::new("./src/templates/", ".hbs")
+    ));
+    
+    if let Err(r) = hbse.reload() {
+        panic!("{}", r.description());
+    }
+    chain.link_after(hbse);
+
+    println!("[+] Listen on localhost:3000");
+    Iron::new(chain).http("localhost:3000").unwrap();
+}
