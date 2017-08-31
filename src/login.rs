5cd3f6698925c5864f0a30605eb32c996ac11665
Chihiro Hasegawa <pgm3rdlinuxor1000@gmail.com>
2017-09-01 08:42:49 +0900

    POSTパラメータは取れる

---
+++ src/login.rs
@@ -0,0 +1,40 @@
+extern crate iron;
+extern crate router;
+extern crate handlebars_iron as hbs;
+extern crate params;
+
+use std::collections::HashMap;
+use std::error::Error;
+use iron::prelude::*;
+use iron::status;
+use router::{Router, url_for};
+use hbs::{Template, HandlebarsEngine, DirectorySource};
+use params::{Params, Value};
+
+pub fn login(req: &mut Request) -> IronResult<Response> {
+
+    println!("[+] Called login");
+    let map = req.get_ref::<Params>().unwrap();
+    
+    let username = match map.find(&["username"]) {
+        Some(&Value::String(ref name))  => {
+            name
+        },
+        _ => {
+            "hoge"
+        }
+    };
+    
+    println!("[+] Username {}", username);
+    let password = match map.find(&["password"]) {
+        Some(&Value::String(ref name))  => {
+            name
+        },
+        _ => {
+            "hoge"
+        }
+    };
+    
+    println!("[+] Password {}", password);
+    return Ok(Response::with((iron::status::Ok)))
+}
