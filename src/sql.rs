8edbaeb61a60bb1ee394eabbbe1b62be9bf6b408
Chihiro Hasegawa <pgm3rdlinuxor1000@gmail.com>
2017-09-03 23:33:57 +0900

    不要なコードの削除

--- src/sql.rs
+++ src/sql.rs
@@ -39,12 +39,6 @@ pub struct UserNames {
     pub usernames: Vec<String>
 }
     
-
-
-pub fn create_db() {
-    
-}
-
 /*
         conn.execute("CREATE TABLE user (
                   id              INTEGER PRIMARY KEY,
@@ -71,79 +65,3 @@ pub fn create_db() {
     
     // from post parameter         
      */
-
-pub fn register_get(req: &mut Request) -> IronResult<Response> {
-    
-    let mut resp = Response::new();
-    let mut data = HashMap::new();
-    
-    data.insert(String::from("register_path"),
-                format!("{}", url_for(req, "register", HashMap::new())));
-    data.insert(String::from("title"),
-                format!("{}", url_for(req, "register", HashMap::new())));
-        
-    resp.set_mut(Template::new("register", data)).set_mut(status::Ok);
-    
-    return Ok(resp);
-}
-
-pub fn register(req: &mut Request) -> IronResult<Response> {
-
-    let conn = Connection::open("./sqlite3.db").unwrap();
-    
-    println!("[+] Called register");
-    {
-        let map = req.get_ref::<Params>().unwrap();
-        
-        let username = match map.find(&["username"]) {
-        Some(&Value::String(ref name))  => {
-            name
-        },
-        _ => {
-            "fail"
-        }
-        };
-    println!("[+] Username {}", username);
-    
-    let password = match map.find(&["password"]) {
-        Some(&Value::String(ref name))  => {
-            name
-        },
-        _ => {
-            "fail"
-        }
-    };
-    
-    println!("[+] Password {}", password);
-    
-    let email = match map.find(&["email"]) {
-        Some(&Value::String(ref name))  => {
-            name
-        },
-        _ => {
-            "fail"
-        }
-    };
-    
-    println!("[+] Email {}", email);
-
-    // to_string() means &str to std::string::String;
-    let me = User {
-        id: 0,
-        username: username.to_string(),
-        password: password.to_string(),
-        email: email.to_string(),
-        permission: 1
-    };
-
-        conn.execute("INSERT INTO user (username, password, email, permission)
-                  VALUES (?1, ?2, ?3, ?4)",
-                 &[&me.username, &me.password, &me.email, &me.permission]).unwrap();
-    }
-    
-    let ref top_url = url_for(req, "index", HashMap::new());
-    return Ok(Response::with((status::Found, Redirect(top_url.clone()))))
-        
-}
-
-
