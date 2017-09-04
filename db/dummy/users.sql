eb06a7e4c714f9c59054a1dfcbe342be0aa66afb
Chihiro Hasegawa <pgm3rdlinuxor1000@gmail.com>
2017-09-04 13:32:48 +0900

    dummyユーザの追加

--- db/dummy/users.sql
+++ db/dummy/users.sql
@@ -10,3 +10,42 @@ INSERT INTO users (email, username, permission, bio, graphic, password)
 
 INSERT INTO users (email, username, permission, bio, graphic, password)
   VALUES ("uplus@cpaw.com", "uplus", 0, "good", "uplus", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("ikuta@cpaw.com", "ikuta", 1, "good", "ikuta", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("hoyotaka@cpaw.com", "hoyotaka", 1, "good", "hoyotaka", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("n4nu@cpaw.com", "n4nu", 1, "good", "n4nu", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("nomuken@cpaw.com", "nomuken", 1, "good", "nomuken", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("iwasi@cpaw.com", "iwasi", 1, "good", "iwasi", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("choll@cpaw.com", "choll", 1, "good", "choll", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("takt@cpaw.com", "takt", 1, "good", "takt", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("ariake@cpaw.com", "ariake", 1, "good", "ariake", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("fujihiro@cpaw.com", "fujihiro", 1, "good", "fujihiro", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("ryou@cpaw.com", "ryou", 1, "good", "ryou", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+INSERT INTO users (email, username, permission, bio, graphic, password)
+  VALUES ("yuta@cpaw.com", "yuta", 1, "good", "yuta", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
+
+
+
+
+
+
