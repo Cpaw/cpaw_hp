e7f6e63f8710a40c20472406305eb34dec25247c
uplus <uplus.e10@gmail.com>
2017-09-06 15:19:11 +0900

    usersテーブルを更新

--- db/dummy/empty.sql
+++
@@ -1,3 +0,0 @@
--- Password is "foobar"
-INSERT INTO users (email, username, permission, bio, graphic, password)
-  VALUES ("tom@cpaw.com", "tom", 1, "good", "tom", "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425");
